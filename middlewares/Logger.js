2886a33658c08db39ad3ec2ee3ecc3da9e742ce3
Maxime <maxime.eleonore@epitech.eu>
2023-11-20 10:27:29 +0100

    commit

--- middlewares/Logger.js
+++ middlewares/Logger.js
@@ -1,6 +1,11 @@
 import { createWriteStream } from 'fs';
 import { join } from 'path';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const logStream = createWriteStream(join(__dirname, '../log/server.log'), { flags: 'a' });
 
 const logger = (req, res, next) => {
