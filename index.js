c865f4a18ad6b6540f0bed709b17265778fe8d0c
Maxime <maxime.eleonore@epitech.eu>
2023-12-04 17:01:52 +0100

    feat: cron task to update crypto datas and self signed ssl certificate

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ const corsOptions = {
         } else {
             callback(new Error('Not allowed by CORS'))
         }
-    }
+    },
+    credentials: true
 }
 app.use(cors(corsOptions));
 app.use(passport.initialize(undefined));
